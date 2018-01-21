5a4e8d7dd43a30bec180f4ceb67e763b39b84ef3
DAUTRY Loïc <16049822+L0Lock@users.noreply.github.com>
2018-01-22 00:41:22 +0100

    Create OCES_main.js

---
+++ OCES_main.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name        OC Enhancer Script
+// @author      -L0Lock-
+// @namespace   artstation.com/dautryloic
+// @description Enhance OC for my use.
+// @include     *openclassrooms.com/*
+// @version     0.1
+// @grant       GM_xmlhttpRequest
+// @grant       GM_getValue
+// @grant       GM_setValue
+// @require     http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js
+// ==/UserScript==
+
+var pElt = document.querySelector("p");
+pElt.style.color = "red";
+pElt.style.margin = "50px";
